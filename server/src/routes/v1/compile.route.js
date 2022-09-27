cb611d542b90d5e65c2c75d62ca8832c1a0a5cf7
tumolaha <tu30380@gmail.com>
2022-09-27 20:31:13 +0700

    fix

--- server/src/routes/v1/compile.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { CompileController } from '../../controllers/compile.controller';
-const router = express.Router();
-
-router.route('/python').post(CompileController.pythonCompile);
-router.route('/cPlus').post(CompileController.cPlusCompile);
-router.route('/java').post(CompileController.javaCompile);
-
-export const CompileRoutes = router;
