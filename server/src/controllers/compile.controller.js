c06bd9c8ebcf21a71e77fdfeb9323f37261a5c06
VuThanhSang <zimb240@gmail.com>
2022-11-20 13:25:25 +0700

    compile

--- server/src/controllers/compile.controller.js
+++ server/src/controllers/compile.controller.js
@@ -3,125 +3,135 @@ import { HttpStatusCode } from '../utilities/constants';
 import { cppCompileService } from '../services/cppCompile.service';
 import { csCompileService } from '../services/csCompile.service';
 import { pythonCompileService } from '../services/pythonCompile.service';
-const pythonCompile = async (req, res) => {
-    try {
-        //if windows
-        var envData = { OS: 'windows' };
-        await pythonCompileService.compilePython(envData, req.body.code, function (data) {
-            res.status(HttpStatusCode.OK).json({ data: data });
-        });
-    } catch (error) {
-        res.status(HttpStatusCode.INTERNAL_SERVER).json({
-            error: new Error(error).message,
-        });
-    }
-};
 
-const pythonCompileWithInput = async (req, res) => {
-    try {
-        //if windows
-        var envData = { OS: 'windows' };
-        await pythonCompileService.compilePythonWithInput(envData, req.body.code, req.body.input, function (data) {
-            res.status(HttpStatusCode.OK).json({ data: data });
-        });
-    } catch (error) {
-        res.status(HttpStatusCode.INTERNAL_SERVER).json({
-            error: new Error(error).message,
-        });
+const compileWithInput = async (req, res) => {
+    switch (req.body.chooseLanguage) {
+        case 'python':
+            try {
+                //if windows
+                var envData = { OS: 'windows' };
+                await pythonCompileService.compilePythonWithInput(
+                    envData,
+                    req.body.code,
+                    req.body.input,
+                    function (data) {
+                        res.status(HttpStatusCode.OK).json({ data: data });
+                    },
+                );
+            } catch (error) {
+                res.status(HttpStatusCode.INTERNAL_SERVER).json({
+                    error: new Error(error).message,
+                });
+            }
+            break;
+        case 'cs':
+            try {
+                const envData = { OS: 'windows' };
+                csCompileService.compileCSWithInput(envData, req.body.code, req.body.input, function (data) {
+                    res.status(HttpStatusCode.OK).json({ data: data });
+                });
+            } catch (error) {
+                res.status(HttpStatusCode.INTERNAL_SERVER).json({
+                    error: new Error(error).message,
+                });
+            }
+            break;
+        case 'cpp':
+            try {
+                //if windows
+                var envData = { OS: 'windows' };
+                //else
+                cppCompileService.compileCPPWithInput(envData, req.body.code, req.body.input, function (data) {
+                    console.log(data);
+                    res.status(HttpStatusCode.OK).json({ data: data });
+                });
+            } catch (error) {
+                res.status(HttpStatusCode.INTERNAL_SERVER).json({
+                    error: new Error(error).message,
+                });
+            }
+            break;
+        case 'java':
+            try {
+                //if windows
+                var envData = { OS: 'windows' };
+                //else
+                JavaCompileService.compileJavaWithInput(envData, req.body.code, req.body.input, function (data) {
+                    console.log(data);
+                    res.status(HttpStatusCode.OK).json({ data: data });
+                });
+            } catch (error) {
+                res.status(HttpStatusCode.INTERNAL_SERVER).json({
+                    error: new Error(error).message,
+                });
+            }
+            break;
+        default:
+            break;
     }
 };
-const csCompile = async (req, res) => {
-    try {
-        const envData = { OS: 'windows' };
-        csCompileService.compileCS(envData, req.body.code, function (data) {
-            res.status(HttpStatusCode.OK).json({ data: data });
-        });
-    } catch (error) {
-        res.status(HttpStatusCode.INTERNAL_SERVER).json({
-            error: new Error(error).message,
-        });
+const compile = async (req, res) => {
+    switch (req.body.chooseLanguage) {
+        case 'python':
+            try {
+                //if windows
+                var envData = { OS: 'windows' };
+                await pythonCompileService.compilePython(envData, req.body.code, function (data) {
+                    res.status(HttpStatusCode.OK).json({ data: data });
+                });
+            } catch (error) {
+                res.status(HttpStatusCode.INTERNAL_SERVER).json({
+                    error: new Error(error).message,
+                });
+            }
+            break;
+        case 'cs':
+            try {
+                const envData = { OS: 'windows' };
+                csCompileService.compileCS(envData, req.body.code, function (data) {
+                    res.status(HttpStatusCode.OK).json({ data: data });
+                });
+            } catch (error) {
+                res.status(HttpStatusCode.INTERNAL_SERVER).json({
+                    error: new Error(error).message,
+                });
+            }
+            break;
+        case 'cpp':
+            try {
+                //if windows
+                var envData = { OS: 'windows' };
+                //else
+                cppCompileService.compileCPP(envData, req.body.code, function (data) {
+                    console.log(data);
+                    res.status(HttpStatusCode.OK).json({ data: data });
+                });
+            } catch (error) {
+                res.status(HttpStatusCode.INTERNAL_SERVER).json({
+                    error: new Error(error).message,
+                });
+            }
+            break;
+        case 'java':
+            try {
+                //if windows
+                var envData = { OS: 'windows' };
+                //else
+                JavaCompileService.compileJava(envData, req.body.code, function (data) {
+                    console.log(data);
+                    res.status(HttpStatusCode.OK).json({ data: data });
+                });
+            } catch (error) {
+                res.status(HttpStatusCode.INTERNAL_SERVER).json({
+                    error: new Error(error).message,
+                });
+            }
+            break;
+        default:
+            break;
     }
 };
-const csCompileWithInput = async (req, res) => {
-    try {
-        const envData = { OS: 'windows' };
-        csCompileService.compileCSWithInput(envData, req.body.code, req.body.input, function (data) {
-            res.status(HttpStatusCode.OK).json({ data: data });
-        });
-    } catch (error) {
-        res.status(HttpStatusCode.INTERNAL_SERVER).json({
-            error: new Error(error).message,
-        });
-    }
-};
-const cPlusCompile = async (req, res) => {
-    try {
-        //if windows
-        var envData = { OS: 'windows' };
-        //else
-        cppCompileService.compileCPP(envData, req.body.code, function (data) {
-            console.log(data);
-            res.status(HttpStatusCode.OK).json({ data: data });
-        });
-    } catch (error) {
-        res.status(HttpStatusCode.INTERNAL_SERVER).json({
-            error: new Error(error).message,
-        });
-    }
-};
-const cPlusCompileWithInput = async (req, res) => {
-    try {
-        //if windows
-        var envData = { OS: 'windows' };
-        //else
-        cppCompileService.compileCPPWithInput(envData, req.body.code, req.body.input, function (data) {
-            console.log(data);
-            res.status(HttpStatusCode.OK).json({ data: data });
-        });
-    } catch (error) {
-        res.status(HttpStatusCode.INTERNAL_SERVER).json({
-            error: new Error(error).message,
-        });
-    }
-};
-const javaCompile = async (req, res) => {
-    try {
-        //if windows
-        var envData = { OS: 'windows' };
-        //else
-        JavaCompileService.compileJava(envData, req.body.code, function (data) {
-            console.log(data);
-            res.status(HttpStatusCode.OK).json({ data: data });
-        });
-    } catch (error) {
-        res.status(HttpStatusCode.INTERNAL_SERVER).json({
-            error: new Error(error).message,
-        });
-    }
-};
-const javaCompileWithInput = async (req, res) => {
-    try {
-        //if windows
-        var envData = { OS: 'windows' };
-        //else
-        JavaCompileService.compileJavaWithInput(envData, req.body.code, req.body.input, function (data) {
-            console.log(data);
-            res.status(HttpStatusCode.OK).json({ data: data });
-        });
-    } catch (error) {
-        res.status(HttpStatusCode.INTERNAL_SERVER).json({
-            error: new Error(error).message,
-        });
-    }
-};
-
 export const CompileController = {
-    pythonCompile,
-    pythonCompileWithInput,
-    cPlusCompile,
-    javaCompile,
-    javaCompileWithInput,
-    csCompile,
-    csCompileWithInput,
-    cPlusCompileWithInput,
+    compile,
+    compileWithInput,
 };
